f7d4b5b580b20220813693c699500f008b4cc88a
New Planet <newplanetfigure@fastmail.com>
2023-05-24 19:49:59 +0200

    auth

--- main.py
+++ main.py
@@ -1,48 +1,141 @@
 import os
+import bcrypt
+import jwt
+import datetime
+
+from dotenv import load_dotenv
 from twilio.rest import Client
 from typing import Optional
-from fastapi import FastAPI
-from fastapi import Form
-from fastapi.responses import HTMLResponse
-from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+
+from fastapi import FastAPI, Form, HTTPException, Depends, Cookie, Body, Request
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.responses import HTMLResponse, RedirectResponse
+
+load_dotenv()
 
 app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 account_sid = os.environ.get("ACCOUNT_SID")
 api_key = os.environ.get("API_KEY")
 api_secret = os.environ.get("API_SECRET")
+jwt_secret = os.environ.get("JWT_SECRET")
+password_hashed = os.environ.get("PASSWORD_HASHED").encode()
 
 client = Client(api_key, api_secret, account_sid)
+
+
+def verify_password(password_plain, password_hashed):
+    return bcrypt.checkpw(password_plain, password_hashed)
+
+
+def create_access_token():
+    payload = {
+        "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+        + datetime.timedelta(seconds=3600)
+    }
+    return jwt.encode(payload, jwt_secret, algorithm="HS256")
+
+
+def decode_access_token(encoded):
+    try:
+        return jwt.decode(encoded, jwt_secret, algorithms=["HS256"])
+    except:
+        raise HTTPException(
+            status_code=302, detail="can't decode token", headers={"Location": "/login"}
+        )
+
+
+def get_decoded_token(request: Request):
+    access_token = request.cookies.get("access_token")
+    if not access_token:
+        raise HTTPException(
+            status_code=302, detail="no cookie", headers={"Location": "/login"}
+        )
+
+    decoded_token = decode_access_token(access_token)
+
+    return decoded_token
+
 
 @app.get("/health")
 async def health():
     return {"message": "ok"}
 
-@app.post('/message')
-async def message_send(from_: str = Form(...), to: str = Form(...), body: str = Form(...)):
-    client.messages.create(
-        from_=from_,
-        to=to,
-        body=body
-    )
-    return RedirectResponse(url='/messages', status_code=303)
-    
+
+@app.get("/login")
+async def login_main():
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="table-responsive p-3">
+            <h1>Login</h1>
+            <form action="/login" method="post">
+                <div class="form-group">
+                   <label for="password">Password:</label>
+                   <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </body>
+    """
+
+    return HTMLResponse(content=html_content)
+
+
+@app.post("/login")
+async def login(password: str = Form(...)):
+    password = password.encode()
+    if not verify_password(password, password_hashed):
+        raise HTTPException(
+            status_code=302, detail="password no good", headers={"Location": "/login"}
+        )
+
+    access_token = create_access_token()
+
+    response = RedirectResponse(url="/messages", status_code=302)
+    response.set_cookie(key="access_token", value=access_token)
+    return response
+
+
+@app.post("/message")
+async def message_send(
+    from_: str = Form(...),
+    to: str = Form(...),
+    body: str = Form(...),
+    decoded_token: str = Depends(get_decoded_token),
+):
+    client.messages.create(from_=from_, to=to, body=body)
+    return RedirectResponse(url="/messages", status_code=303)
+
 
 @app.get("/messages-json")
-async def messages_json():
+async def messages_json(decoded_token: str = Depends(get_decoded_token)):
     messages = client.messages.list()
     if len(messages) == 0:
         return {"message": "No messages"}
-    
-    out = ""
+
+    out = []
     for sms in client.messages.list():
-        out += f"{sms.from_} -> {sms.to}, sent on {sms.date_sent}, num_media={sms.num_media}"
-        out += f"=====\n{sms.body}\n====="
+        out_i = {}
+        out_i["from_"] = sms.from_
+        out_i["to"] = sms.to
+        out_i["num_media"] = sms.num_media
+        out_i["body"] = sms.body
+        out.append(out_i)
 
     return {"message": out}
 
+
 @app.get("/messages")
-async def messages():
+async def messages(decoded_token: str = Depends(get_decoded_token)):
     messages = client.messages.list()
 
     body = ""
@@ -118,4 +211,3 @@ async def messages():
     """
 
     return HTMLResponse(content=html_content)
-
