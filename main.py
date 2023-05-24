a0a15f3c6376f1f87d978d865a3983ee8bee7a97
New Planet <newplanetfigure@fastmail.com>
2023-05-24 04:55:16 +0200

    simple html

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from twilio.rest import Client
 from typing import Optional
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -15,8 +16,8 @@ client = Client(api_key, api_secret, account_sid)
 async def health():
     return {"message": "ok"}
 
-@app.get("/messages")
-async def root():
+@app.get("/messages-json")
+async def messages_json():
     messages = client.messages.list()
     if len(messages) == 0:
         return {"message": "No messages"}
@@ -28,4 +29,46 @@ async def root():
         out += f"=====\n{sms.body}\n====="
 
     return {"message": out}
+
+@app.get("/messages", response_class=HTMLResponse)
+async def messages():
+    messages = client.messages.list()
+
+    body = ""
+    for sms in client.messages.list():
+        body += "<tr>"
+        body += f"<td>{sms.from_}</td>"
+        body += f"<td>{sms.to}</td>"
+        body += f"<td>{sms.date_sent}</td>"
+        body += f"<td>{sms.body}</td>"
+        body += f"<td>{sms.num_media}</td>"
+        body += "</tr>"
+
+    html_content = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>From</th>
+                        <th>To</th>
+                        <th>Date sent</th>
+                        <th>Body</th>
+                        <th>Num media</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {body}
+                </tbody>
+            </table>
+        </div>
+    </body>
+    """
+
+    return HTMLResponse(content=html_content)
 
